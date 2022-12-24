0d8f65307c4f13a2a0e1eee39eb00eec6a86dc75
Hewerton <hewertonlourenco@gmail.com>
2022-12-24 15:45:40 -0300

    feat: some changes in the layout

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'epilogue': ['Epilogue', 'sans-serif'],
+    }
+  },
+  plugins: [require("daisyui")],
+}
